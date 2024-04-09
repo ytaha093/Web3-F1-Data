b5fc5bf3f7d161bab6fa44d6c8e74acf32d04195
Yaseen <yaseen_taha123@hotmail.com>
2024-04-08 23:44:29 -0600

    minor fixing and comments

--- my-project/src/components/HomeView1.jsx
+++ my-project/src/components/HomeView1.jsx
@@ -41,7 +41,7 @@ export const HomeView1 = (props) => {
 
 
   return (
-    <div className="flex flex-wrap">
+    <div className="flex flex-wrap bg-cover bg-[url('/bgg.jpg')]">
       <Header supabase={props.supabase} />
       <SideBar supabase={props.supabase} />
       {displayMainCard()}
